824d30b4fe12ac6eac4687a06097a371b06d5683
Sahid Reza <sahidreza976@gmail.com>
2026-08-12 07:20:54 +0530

    Add the array

--- src/main/java/org/example/searching_In_Array.java
+++ src/main/java/org/example/searching_In_Array.java
@@ -1,7 +1,13 @@
 package org.example;
 
 public class searching_In_Array {
+
+
     public static void main(String[] args) {
 
+        int arr[] = { 2, 4, 5, 7, 7, 6 };
+        int x = 6;
+        int k = 2;
+        int n = arr.length;
     }
 }
