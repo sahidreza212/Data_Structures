5be7116232bcf2888db9213b547c768a07d6a16f
Sahid Reza <sahidreza976@gmail.com>
2026-08-12 06:48:14 +0530

    Add this portion

--- src/main/java/org/example/Common_Element_In_3.java
+++ src/main/java/org/example/Common_Element_In_3.java
@@ -1,7 +1,12 @@
 package org.example;
 
 public class Common_Element_In_3 {
+
     public static void main(String[] args) {
+
+        int[] a = {1, 5, 10, 20, 30};
+        int[] b = {5, 13, 15, 20};
+        int[] c = {5, 20};
 
     }
 }
