682c19ec4545ef7ff2d49d3e981c2cf061c64de4
Sahid Reza <sahidreza976@gmail.com>
2026-08-13 06:47:51 +0530

    Add the findPair function

--- src/main/java/org/example/Pair_With_Given_Difference.java
+++ src/main/java/org/example/Pair_With_Given_Difference.java
@@ -1,6 +1,9 @@
 package org.example;
 
 public class Pair_With_Given_Difference {
+    public static boolean findPair(int arr[] , int x){
+
+    }
     public static void main(String[] args) {
 
         int arr[] = {5,20,3,2,50,80};
