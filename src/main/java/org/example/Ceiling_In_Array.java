45e583a2e127ddb7860f7c205cda3b55bfc2d6e4
Sahid Reza <sahidreza976@gmail.com>
2026-08-13 06:34:49 +0530

    Add the final cimmit

--- src/main/java/org/example/Ceiling_In_Array.java
+++ src/main/java/org/example/Ceiling_In_Array.java
@@ -3,14 +3,16 @@ package org.example;
 public class Ceiling_In_Array {
 
     public static int ceilingSearch(int arr[] , int x){
-        // X is less then the first element of the array
-        if(x <= arr[0]);
-        return 0;
+        // X is less than the first element of the array
+        if(x <= arr[0]) {
+            return 0;
+        }
 
         for(int i = 0; i<arr.length-1;i++){
             // when we find out the exact element
-            if(arr[i]==x);
-            return i;
+            if(arr[i]==x) {
+                return i;
+            }
 
             // when the x in the range of i and i+1
             if(arr[i]<x && arr[i+1]>=x){
@@ -21,13 +23,13 @@ public class Ceiling_In_Array {
     }
     public static void main(String[] args) {
         int []arr = {1,2,5,10,10,12,19};
-        int x = 3;
+        int x = 20;
 
         int index = ceilingSearch(arr,x);
         if(index == -1){
             System.out.println("The Ceiling of "+x+" is does not exists!");
         }else {
-            System.out.println("The Ceiling of "+x+ "is:"+arr[index]);
+            System.out.println("The Ceiling of "+x+ " is: "+arr[index]);
         }
     }
 }
