d0f7895dccc1787eff14f434a0ec78de5cea7f54
Sahid Reza <sahidreza976@gmail.com>
2026-08-13 06:46:46 +0530

    Add the array

--- src/main/java/org/example/Pair_With_Given_Difference.java
+++ src/main/java/org/example/Pair_With_Given_Difference.java
@@ -3,5 +3,8 @@ package org.example;
 public class Pair_With_Given_Difference {
     public static void main(String[] args) {
 
+        int arr[] = {5,20,3,2,50,80};
+        int x = 78;
+
     }
 }
