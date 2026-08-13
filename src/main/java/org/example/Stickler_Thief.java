04d7c56b7f5eb69f41d0e5e5ad66ec079b5a2ef2
Sahid Reza <sahidreza976@gmail.com>
2026-08-13 20:21:05 +0530

    Add the final commit

--- src/main/java/org/example/Stickler_Thief.java
+++ src/main/java/org/example/Stickler_Thief.java
@@ -1,7 +1,28 @@
 package org.example;
 
 public class Stickler_Thief {
-    public static void main(String[] args) {
 
+    public static int maxLootRec(int arr[] , int n){
+        if(n<=0){
+            return 0;
+        }
+        if(n==0){
+            return arr[0];
+        }
+
+        int pick = arr[n-1]+maxLootRec(arr,n-2);
+        int notPick = maxLootRec(arr,n-1);
+
+        return Math.max(pick,notPick);
+    }
+
+    public static int findMaxSum(int [] arr){
+        int n = arr.length;
+
+        return maxLootRec(arr,n);
+    }
+    public static void main(String[] args) {
+        int [] arr = {6,5,5,7,4};
+        System.out.println(findMaxSum(arr));
     }
 }
