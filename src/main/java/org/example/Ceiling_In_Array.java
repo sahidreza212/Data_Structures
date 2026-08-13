32d7b9e845dd0bdfa3c9175b2b465e69704f93e4
Sahid Reza <sahidreza976@gmail.com>
2026-08-13 06:23:48 +0530

    Add CeilingSearch function

--- src/main/java/org/example/Ceiling_In_Array.java
+++ src/main/java/org/example/Ceiling_In_Array.java
@@ -1,6 +1,11 @@
 package org.example;
 
 public class Ceiling_In_Array {
+
+    public static int ceilingSearch(int arr[] , int x){
+        if(x <= arr[0]);
+        return 0;
+    }
     public static void main(String[] args) {
         int []arr = {1,2,5,10,10,12,19};
         int x = 3;
