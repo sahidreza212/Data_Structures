a9330e49094678a85b255011382a9dda5e85c007
Sahid Reza <sahidreza976@gmail.com>
2026-08-13 06:50:46 +0530

    Add the full function

--- src/main/java/org/example/Pair_With_Given_Difference.java
+++ src/main/java/org/example/Pair_With_Given_Difference.java
@@ -3,6 +3,17 @@ package org.example;
 public class Pair_With_Given_Difference {
     public static boolean findPair(int arr[] , int x){
 
+        int n = arr.length;
+        for (int i = 0; i<n;i++){
+            for(int j = 0;j<n;j++){
+
+                // find the absolute difference match
+                if(Math.abs(arr[i] - arr[j]) == x){
+                    return true;
+                }
+            }
+        }
+        return false;
     }
     public static void main(String[] args) {
 
