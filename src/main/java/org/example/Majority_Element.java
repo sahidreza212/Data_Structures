7636a2051f72d3aa787d39ea082a743de4a01e89
Sahid Reza <sahidreza976@gmail.com>
2026-08-13 19:02:41 +0530

    Add the final commit

--- src/main/java/org/example/Majority_Element.java
+++ src/main/java/org/example/Majority_Element.java
@@ -1,4 +1,26 @@
 package org.example;
 
 public class Majority_Element {
+    public static int majorityElement(int arr[]){
+
+        int n = arr.length;
+        int count = 0;
+
+        for(int i = 0; i<n;i++){
+            for(int j = 0;j<n;j++){
+
+                if(arr[i] == arr[j]){
+                    count++;
+                }
+            }
+            if(count > n/2){
+                return arr[i];
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int []arr = {1,1,2,1,3,5,1};
+        System.out.println(majorityElement(arr));
+    }
 }
