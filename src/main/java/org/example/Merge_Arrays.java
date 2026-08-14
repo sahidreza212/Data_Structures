11fb847eecc4903ffcc5a5bf78db892238299080
Sahid Reza <sahidreza976@gmail.com>
2026-08-14 06:39:39 +0530

    Add the arrays

--- src/main/java/org/example/Merge_Arrays.java
+++ src/main/java/org/example/Merge_Arrays.java
@@ -1,7 +1,11 @@
 package org.example;
 
 public class Merge_Arrays {
+
+
     public static void main(String[] args) {
 
+        int a[] = {1,5,9,10,15,20};
+        int b[] = {2,3,8,13};
     }
 }
