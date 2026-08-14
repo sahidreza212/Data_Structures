2164378242e55bdb49d2647d66f7ecc87255dd7e
Sahid Reza <sahidreza976@gmail.com>
2026-08-14 06:42:40 +0530

    add the for loop

--- src/main/java/org/example/Merge_Arrays.java
+++ src/main/java/org/example/Merge_Arrays.java
@@ -4,6 +4,17 @@ public class Merge_Arrays {
 
     public static void mergeArrays(int a[] , int b[]){
 
+        for(int i = b.length-1;i>=0;i--){
+
+            if(a[a.length-1] > b[i]){
+
+                int last = a[a.length-1];
+                int j = a.length-2;
+
+
+            }
+        }
+
     }
 
     public static void main(String[] args) {
