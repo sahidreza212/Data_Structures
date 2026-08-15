7f2f411586fc801426322008d33b0b14288d2104
Sahid Reza <sahidreza976@gmail.com>
2026-08-15 18:57:52 +0530

    Add the full function

--- src/main/java/org/example/Minimum_Cost.java
+++ src/main/java/org/example/Minimum_Cost.java
@@ -6,7 +6,13 @@ public class Minimum_Cost {
 
     public static int minCost(int arr[]){
         Arrays.sort(arr);
+        int median = arr[arr.length / 2];
+        int cost = 0;
+        for(int x : arr){
 
+            cost += Math.abs(x-median);
+        }
+        return cost;
     }
     public static void main(String[] args) {
 
