d98dc9e6da90334b912a0e25574023bf90f3e8eb
Sahid Reza <sahidreza976@gmail.com>
2026-08-15 07:04:07 +0530

    Add the findDuplicate method

--- src/main/java/org/example/Find_Duplicates.java
+++ src/main/java/org/example/Find_Duplicates.java
@@ -1,7 +1,12 @@
 package org.example;
 
+import java.util.List;
+
 public class Find_Duplicates {
 
+    public static List<Integer> findDuplicates(int arr[]){
+
+    }
     public static void main(String[] args) {
 
         int arr[] = { 1,6,5,2,3,3,2};
