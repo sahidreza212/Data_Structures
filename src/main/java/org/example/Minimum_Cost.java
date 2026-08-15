1b8924ff3425da8152e1c1a3a1cea881dc729884
Sahid Reza <sahidreza976@gmail.com>
2026-08-15 18:55:21 +0530

    Add the minCost function

--- src/main/java/org/example/Minimum_Cost.java
+++ src/main/java/org/example/Minimum_Cost.java
@@ -1,7 +1,15 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Minimum_Cost {
+
+    public static int minCost(int arr[]){
+        Arrays.sort(arr);
+
+    }
     public static void main(String[] args) {
 
+        int arr[] = {1,100,101};
     }
 }
