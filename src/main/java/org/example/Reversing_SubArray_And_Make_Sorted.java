b2dfdb53d633965329c84cb7429ed695236ae8ad
Sahid Reza <sahidreza976@gmail.com>
2026-08-15 19:57:57 +0530

    Add the full function

--- src/main/java/org/example/Reversing_SubArray_And_Make_Sorted.java
+++ src/main/java/org/example/Reversing_SubArray_And_Make_Sorted.java
@@ -1,9 +1,48 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Reversing_SubArray_And_Make_Sorted {
 
     public static boolean checkReverse(int arr[] , int n){
 
+        int [] temp = new int[n];
+        for(int i = 0;i<n; i++){
+            temp[i] = arr[i];
+        }
+
+        Arrays.sort(temp);
+
+        // finding the first mismatch
+        int front;
+        for(front = 0;front < n ; front++){
+            if(temp[front] != arr[front]){
+                break;
+            }
+        }
+
+        // finding the last mismatch
+        int back;
+        for(back = n-1; back>=0;back--){
+            if(temp[back] != arr[back]){
+                break;
+            }
+        }
+
+        // if the whole array is sorted
+        if(front >= back){
+            return true;
+        }
+
+        // checking the array is decreasing or not
+
+        while (front < back){
+            if(arr[front] <= arr[front+1]){
+                return false;
+            }
+            front++;
+        }
+        return true;
     }
     public static void main(String[] args) {
 
