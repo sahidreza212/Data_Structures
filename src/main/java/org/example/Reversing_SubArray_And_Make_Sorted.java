552c03a88cf783259dee588fdfdd34f4cb3e5438
Sahid Reza <sahidreza976@gmail.com>
2026-08-15 19:51:01 +0530

    Add the checkReverse function

--- src/main/java/org/example/Reversing_SubArray_And_Make_Sorted.java
+++ src/main/java/org/example/Reversing_SubArray_And_Make_Sorted.java
@@ -1,7 +1,13 @@
 package org.example;
 
 public class Reversing_SubArray_And_Make_Sorted {
+
+    public static boolean checkReverse(int arr[] , int n){
+
+    }
     public static void main(String[] args) {
 
+        int arr [] = {1,2,5,4,3};
+        int n = arr.length;
     }
 }
