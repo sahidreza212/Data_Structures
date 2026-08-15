11b885710fcc7337bc20db8bba6806e869e2833a
Sahid Reza <sahidreza976@gmail.com>
2026-08-15 07:56:23 +0530

    Add the full function

--- src/main/java/org/example/Product_Array_Except_Self.java
+++ src/main/java/org/example/Product_Array_Except_Self.java
@@ -1,9 +1,25 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Product_Array_Except_Self {
 
     public static  int[] productExceptSelf(int arr[]){
 
+        int n = arr.length;
+        int [] res = new int[n];
+        Arrays.fill(res,1);
+
+        for(int i = 0;i<n;i++){
+
+            for(int j = 0; j< n;j++){
+
+                if(arr[i] != arr[j]){
+                    res[i] *= arr[j];
+                }
+            }
+        }
+        return res;
     }
     public static void main(String[] args) {
 
