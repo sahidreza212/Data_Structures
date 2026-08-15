b9ab9b11918d54b9af7ab6629ccdfcabc383b4fe
Sahid Reza <sahidreza976@gmail.com>
2026-08-15 07:52:57 +0530

    add the function product Except Self

--- src/main/java/org/example/Product_Array_Except_Self.java
+++ src/main/java/org/example/Product_Array_Except_Self.java
@@ -1,8 +1,13 @@
 package org.example;
 
 public class Product_Array_Except_Self {
+
+    public static  int[] productExceptSelf(int arr[]){
+
+    }
     public static void main(String[] args) {
 
+        int arr[] = {10,3,5,6,2};
     }
 
 }
