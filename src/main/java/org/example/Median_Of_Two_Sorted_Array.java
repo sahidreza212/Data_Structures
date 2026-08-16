33937ee48a6e2bdba03bb1d7dd1fa8ee66a85380
Sahid Reza <sahidreza976@gmail.com>
2026-08-16 09:29:32 +0530

    Add the full function of median of 2 array

--- src/main/java/org/example/Median_Of_Two_Sorted_Array.java
+++ src/main/java/org/example/Median_Of_Two_Sorted_Array.java
@@ -1,9 +1,24 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Median_Of_Two_Sorted_Array {
 
     public static double medianOf2(int [] a, int[] b){
 
+        int [] c = new int[a.length + b.length];
+
+        System.arraycopy(a,0,c,0,a.length);
+        System.arraycopy(b,0,c,a.length,b.length);
+
+        Arrays.sort(c);
+        int len = c.length;
+
+        if(len % 2 == 0){
+            return c[len/2]+c[len/2-1];
+        }else {
+            return c[len/2];
+        }
 
     }
     public static void main(String[] args) {
