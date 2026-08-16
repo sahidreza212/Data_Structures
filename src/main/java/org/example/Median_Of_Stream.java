cfa4a8016b14d74d25a7c6b87d18e8f7078ac78a
Sahid Reza <sahidreza976@gmail.com>
2026-08-16 18:40:33 +0530

    Add this getMedian function

--- src/main/java/org/example/Median_Of_Stream.java
+++ src/main/java/org/example/Median_Of_Stream.java
@@ -1,4 +1,15 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class Median_Of_Stream {
+
+    public static ArrayList<Double> getMedian(int arr[]){
+
+    }
+    public static void main(String[] args) {
+        int arr [] = {5, 15, 1, 3, 2, 8};
+
+
+    }
 }
