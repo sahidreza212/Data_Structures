b0d6fbc2a099310a86477e06d1dcc3be794c812b
Sahid Reza <sahidreza976@gmail.com>
2026-08-16 08:39:22 +0530

    Add the fourSum function

--- src/main/java/org/example/Sum_Of_4.java
+++ src/main/java/org/example/Sum_Of_4.java
@@ -1,6 +1,12 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class Sum_Of_4 {
+
+    public static ArrayList<ArrayList<Integer>> fourSum(int arr[] , int target){
+
+    }
     public static void main(String[] args) {
 
         int arr [] = {10,2,3,4,5,7,8};
