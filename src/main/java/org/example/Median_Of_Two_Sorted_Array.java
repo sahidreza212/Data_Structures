bc1f4e1fc277dad2cc6daf657859a0bf1a48e91d
Sahid Reza <sahidreza976@gmail.com>
2026-08-16 09:26:00 +0530

    Add the medianOf2

--- src/main/java/org/example/Median_Of_Two_Sorted_Array.java
+++ src/main/java/org/example/Median_Of_Two_Sorted_Array.java
@@ -1,7 +1,15 @@
 package org.example;
 
 public class Median_Of_Two_Sorted_Array {
+
+    public static double medianOf2(int [] a, int[] b){
+
+
+    }
     public static void main(String[] args) {
+
+        int []a = {-5,3,6,12,15};
+        int []b = {-12,-10,-6,-3,4,10};
 
     }
 }
